28686027c123b59ea90d7243bf63156397f4bde9
Sparue <admin@sparue.com>
2021-08-16 18:52:24 +0530

    first

---
+++ assets/js/main.js
@@ -0,0 +1,114 @@
+// Mobile Menu
+let nav = document.querySelector("nav"); 
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+let logoOne = document.querySelector(".logo");
+
+
+menuBtn.onclick = function (){
+  navBar.classList.add("active");
+  menuBtn.style.opacity ="0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+}
+
+cancelBtn.onclick = function (){
+  navBar.classList.remove("active");
+  menuBtn.style.opacity ="1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+} 
+
+// Side Navigation Bar Close While We click On Navigation Links
+
+let navLinks = document.querySelectorAll(".menu li a");
+for(var i = 0; i < navLinks.length; i++){
+  navLinks[i].addEventListener("click", function(){
+    navBar.classList.remove("active");
+    menuBtn.style.opacity ="1";
+    menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
+  })
+}
+
+
+// Scroll Fixed
+window.onscroll = function (){
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    logoOne.classList.add("active");
+    menuBtn.classList.add("active");
+  }else{
+    nav.classList.remove("sticky");
+    logoOne.classList.remove("active");
+    menuBtn.classList.remove("active");
+  }
+
+// counter on scroll 
+var number = 0;
+var oTop = $('#counter').offset().top - window.innerHeight;
+if (number == 0 && $(window).scrollTop() > oTop) {
+  $('.counter-value').each(function () {
+    var $this = $(this),
+      countTo = $this.attr('data-count');
+    $({
+      countNum: $this.text()
+    }).animate({
+        countNum: countTo
+      },
+
+      {
+
+        duration: 2000,
+        easing: 'swing',
+        step: function () {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function () {
+          $this.text(this.countNum); 
+        }
+
+      });
+  });
+}
+
+
+
+}
+
+  // desclaimer text animation
+  $(document).ready(function(){
+    $("#desc-btn").click(function(){
+      $("#desc-text").slideToggle("slow");
+    });
+  });
+
+// silk slider testmonial
+
+$('.testimonials').slick({
+  dots: false,
+  infinite: true, 
+  slidesToShow: 1, 
+  slidesToScroll:1,
+  autoplay:true,
+  cssEase:'linear',
+  silde:'li',
+  arrows:false,
+});
+
+// back to top button
+
+var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+var rootElement = document.documentElement
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
+
